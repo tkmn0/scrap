1fa1c5e35cabb23fb50010dfddee16ecc2fc6169
tkmn0 <tkmgin0914@gmail.com>
2018-01-06 03:46:10 +0900

    initial commit

---
+++ src/components/ReadNext/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import './style.scss'
+
+class ReadNext extends React.Component {
+  render() {
+    const data = this.props.data
+    const title = get(data, 'meta.title')
+    const author = get(data, 'meta.author')
+
+    return (
+      <div className="footer">
+        <hr className="border-primary" />
+      </div>
+    )
+  }
+}
+
+export default ReadNext
